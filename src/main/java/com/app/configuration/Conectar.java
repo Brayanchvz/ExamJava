c93ef9de94f451772605021784678f7e8c5c4608
Brayanchvz <brayan120@gmail.com>
2021-11-22 23:32:28 -0600

    Config db
    Configuration of db

---
+++ src/main/java/com/app/configuration/Conectar.java
@@ -0,0 +1,28 @@
+package com.app.configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conectar {
+	
+	private static Connection con;
+	private static final String USER="root";
+	private static final String PASS="";
+	private static final String URL="jdbc:mysql://localhost:3306/exam";
+	private static final String DRIVER="com.mysql.cj.jdbc.Driver";
+	public static Connection conectarBD() {
+		try {
+			Class.forName(DRIVER);
+			con = DriverManager.getConnection(URL,USER,PASS);
+		} catch (Exception e) {
+			System.out.println("Error de conexión: "+e);
+		}
+		return con;
+	}
+	
+	public static void name(String[] args) {
+		System.out.println(conectarBD());
+		
+	}
+
+}
